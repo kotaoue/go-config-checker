dbff4788d03560c3206be0873f903c445e3a949e
kota oue <kotaoue@gmail.com>
2022-01-12 23:00:27 +0900

    add main.go

---
+++ tools/config-checker/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if err := Main(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func Main() error {
+	if err := checkNGWords("ok_config"); err != nil {
+		return err
+	}
+	if err := checkNGWords("ng_config"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func checkNGWords(name string) error {
+	fmt.Println(strings.Repeat("-", 64))
+	fmt.Printf("check %s\n", name)
+	file, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+
+	fs := bufio.NewScanner(file)
+	i := 0
+	for fs.Scan() {
+		i++
+		for _, v := range ngWords() {
+			fmt.Println(fs.Text())
+			if strings.Contains(fs.Text(), v) {
+				return fmt.Errorf("%s found on line:%d. origin:%s", v, i, fs.Text())
+			}
+		}
+	}
+	return fs.Err()
+}
+
+func ngWords() []string {
+	return []string{
+		"example.com",
+	}
+}
